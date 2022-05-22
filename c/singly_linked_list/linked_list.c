69d7928b375d33163ceeeba867ff6329b5517319
prashantgandhi27 <prashantgandhi7979@gmail.com>
2022-05-21 21:31:28 -0700

    Singly Linked List Implementation in C.

---
+++ c/singly_linked_list/linked_list.c
@@ -0,0 +1,224 @@
+#include "linked_list.h"
+
+/*!*****************************************************************************
+*  \fn         RING_BUFFER *CreateRingBuffer(uint32_t *buffer, uint32_t ringSize)
+*  \brief      Allocates and initializes a ring
+*
+*  \param[in]  buffer     - pointer to user's buffer
+*  \param[in]  ringSize   - size of user's buffer
+*
+*  \retval     pointer to a ring if successful, else NULL
+*
+******************************************************************************/
+static void *createNode( void )
+{
+    LINKED_LIST *localPtr = (LINKED_LIST *)malloc(sizeof(LINKED_LIST));
+    localPtr->data        = (LINKED_LIST_TYPE)0x00;
+    localPtr->nextNode    = LINKED_LIST_NULL;
+
+    return localPtr;
+}
+
+/******************************************************************************/
+void *InitializeLinkedList( void )
+{
+    return LINKED_LIST_NULL;
+}
+
+/******************************************************************************/
+void InsertItem(void **pLinkedListHead, const LINKED_LIST_TYPE cData)
+{
+    bool status = true;
+
+    LINKED_LIST* localHeadPtr = (LINKED_LIST *)(*pLinkedListHead);
+
+    LINKED_LIST *localTempPtr = (LINKED_LIST *)createNode();
+    localTempPtr->data = cData;
+
+    if ( LINKED_LIST_NULL == localHeadPtr )
+    {
+        *pLinkedListHead = localTempPtr;
+    }
+    else
+    {
+        while ( LINKED_LIST_NULL != localHeadPtr->nextNode )
+        {
+            localHeadPtr = localHeadPtr->nextNode;
+        }
+        
+        localHeadPtr->nextNode = localTempPtr;
+    }
+
+    return;
+}
+
+/******************************************************************************/
+void *SearchItem(void *pLinkedListHead, const LINKED_LIST_TYPE cData)
+{
+    bool found = false;
+
+    while ( LINKED_LIST_NULL != pLinkedListHead )
+    {
+        if ( ((LINKED_LIST *)pLinkedListHead)->data == cData )
+        {
+            found = true;
+            break;
+        }
+        
+        pLinkedListHead = ((LINKED_LIST *)pLinkedListHead)->nextNode;
+    }
+
+    return pLinkedListHead;
+}
+
+/******************************************************************************/
+bool RemoveItem(void **pLinkedListHead, const LINKED_LIST_TYPE cData)
+{
+    bool status = false;
+
+    if ( (pLinkedListHead != LINKED_LIST_NULL ) && 
+       ( (*pLinkedListHead) != LINKED_LIST_NULL ) )
+    {
+        LINKED_LIST *localTempPtr = *pLinkedListHead;
+
+        if ( localTempPtr->data == cData)
+        {
+            status           = true;
+            *pLinkedListHead = localTempPtr->nextNode;
+            free( localTempPtr );
+        }
+        else
+        {
+            while ( LINKED_LIST_NULL != localTempPtr->nextNode )
+            {
+                if ( localTempPtr->nextNode->data == cData )
+                {
+                    status                  = true;
+                    LINKED_LIST *deleteNode = localTempPtr->nextNode;
+                    localTempPtr->nextNode  = localTempPtr->nextNode->nextNode;
+                    free( deleteNode );
+                    break;
+                }
+                else
+                {
+                    localTempPtr = localTempPtr->nextNode;
+                }
+            }
+        }
+    }
+}
+
+/******************************************************************************/
+uint32_t LinkedListSize(const void *pLinkedListHead)
+{
+    uint32_t listSize = 0;
+
+    while ( LINKED_LIST_NULL != pLinkedListHead )
+    {
+        listSize++;
+        pLinkedListHead = ((LINKED_LIST *)pLinkedListHead)->nextNode;
+    }
+
+    return listSize;
+}
+
+/******************************************************************************/
+bool IsLinkedListEmpty(const void *pLinkedListHead)
+{
+    bool empty = false;
+
+    if ( LinkedListSize( pLinkedListHead ) == 0 )
+    {
+        empty = true;
+    }
+
+    return empty;
+}
+
+/******************************************************************************/
+void PrintLinkedList(const void *pLinkedListHead)
+{
+    while ( LINKED_LIST_NULL != pLinkedListHead )
+    {
+        printf("NODE DATA = %d \n", ((LINKED_LIST*)pLinkedListHead)->data);
+        pLinkedListHead = ((LINKED_LIST*)pLinkedListHead)->nextNode;
+    }
+}
+
+/******************************************************************************/
+void DestroyLinkedList(void **pLinkedListHead)
+{
+    LINKED_LIST *localTempPtr  = (LINKED_LIST *)(*pLinkedListHead);
+    LINKED_LIST *LocalDeletPtr = LINKED_LIST_NULL;
+    *pLinkedListHead           = LINKED_LIST_NULL;
+
+    while ( LINKED_LIST_NULL != localTempPtr )
+    {
+        LocalDeletPtr = localTempPtr;
+        localTempPtr  = localTempPtr->nextNode;
+
+        free( LocalDeletPtr );
+    }
+
+    return;
+}
+
+
+/******************************************************************************
+ * Entry point.
+ ******************************************************************************/
+int main( void )
+{
+    LINKED_LIST *TestLinkedList = LINKED_LIST_NULL;
+
+    LINKED_LIST_TYPE linkedListData = 0;
+
+    for ( linkedListData = 0; linkedListData < 10; ++linkedListData )
+    {
+        InsertItem( (void *)&TestLinkedList, linkedListData );
+    }
+
+    PrintLinkedList( TestLinkedList );
+
+    if ( IsLinkedListEmpty( TestLinkedList ) == true )
+    {
+        printf("\n");
+        printf("List is empty. \n");
+        printf("\n");
+    }
+    else
+    {
+        printf("\n");
+        printf("List is not empty. \n");
+        printf("\n");
+    }
+
+    printf("\n");
+    printf("Size of the list = %d \n", LinkedListSize(TestLinkedList));
+    printf("\n");
+
+    if ( RemoveItem( (void *)&TestLinkedList, 0 ) == true )
+    {
+        PrintLinkedList(TestLinkedList);
+        printf("\n");
+        printf("Size of the list after removal = %d \n", LinkedListSize(TestLinkedList));
+        printf("\n");
+    }
+
+    DestroyLinkedList( (void *)&TestLinkedList );
+
+    if ( IsLinkedListEmpty( TestLinkedList ) == true )
+    {
+        printf("\n");
+        printf("After destroy, list is empty. \n");
+        printf("\n");
+    }
+    else
+    {
+        printf("\n");
+        printf("After destroy, list is not empty. \n");
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ c/singly_linked_list/linked_list.h
@@ -0,0 +1,132 @@
+/*!*****************************************************************************
+*  \author     Prashant Gandhi
+*  \date       02/14/2022
+********************************************************************************
+*  \file       ring_buffer.h
+*
+*  \brief      This files contains ring buffer function declaration.
+*
+*  \details    This version of ring buffer does not make use of buffer
+*              size to check whether ring is empty or full. Instead it reserves
+*              one space for that. Meaning, when ring buffer size is given 10,
+*              ring can hold only 9 elements in buffer at a time. 
+*
+*  \note       VERSION - 0.2.0
+*
+*******************************************************************************/
+#ifndef __LINKED_LIST_H__
+#define __LINKED_LIST_H__
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include "linked_list_cfg.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef struct linked_list
+{
+    LINKED_LIST_TYPE data;
+    struct linked_list *nextNode;
+} LINKED_LIST;
+
+
+/*!*****************************************************************************
+*  \brief      Does nothing. Only returns custom NULL pointer. 
+*
+*  \param[in]  None
+*
+*  \retval     custom NULL pointer.
+*
+******************************************************************************/
+void *InitializeLinkedList( void );
+
+/*!*****************************************************************************
+*  \brief      Adds data to the list.
+*
+*  \param[in]  ppLinkedListHead - pointer to the head of the list.
+*  \param[in]  cData            - data that needs to be removed.
+*
+*  \retval     None
+*
+******************************************************************************/
+void InsertItem(void **ppLinkedListHead, const LINKED_LIST_TYPE data);
+
+/*!*****************************************************************************
+*  \brief      Looks for the requested item in the list.
+*
+*  \param[in]  pLinkedListHead - pointer to the head of the list.
+*  \param[in]  cData            - data that needs to be removed.
+*
+*  \retval     if requested node found then pointer to that node, otherwise
+*              NULL.
+*
+******************************************************************************/
+void *SearchItem(void *pLinkedListHead, const LINKED_LIST_TYPE cData);
+
+/*!*****************************************************************************
+*  \brief      Removes/deletes the requested node.
+*
+*  \param[in]  ppLinkedListHead - pointer to the head of the list.
+*  \param[in]  cData            - data that needs to be removed.
+*
+*  \retval     true  - Node containing item was present and node is successfully
+*                      deleted.
+*              false - Node containing item was not present and operation
+*                      failed.
+*
+******************************************************************************/
+bool RemoveItem(void **ppLinkedListHead, const LINKED_LIST_TYPE cData);
+
+/*!*****************************************************************************
+*  \brief      Prints all nodes' data inside the list.
+*
+*  \param[in]  pcLinkedListHead - pointer to the head of the list.
+*
+*  \retval     None
+*
+******************************************************************************/
+void PrintLinkedList(const void *pcLinkedListHead);
+
+/*!*****************************************************************************
+*  \brief      Returns size of the list.
+*
+*  \param[in]  pcLinkedListHead - pointer to the head of the list.
+*
+*  \retval     number of nodes in the list.
+*
+******************************************************************************/
+uint32_t LinkedListSize(const void *pcLinkedListHead);
+
+/*!*****************************************************************************
+*  \brief      Determines whether the input list is empty or not.
+*
+*  \param[in]  pcLinkedListHead - pointer to the head of the list.
+*
+*  \retval     true  - list is empty
+*              false - list is not empty
+*
+******************************************************************************/
+bool IsLinkedListEmpty(const void *pcLinkedListHead);
+
+/*!*****************************************************************************
+*  \brief      Deletes all nodes from the list.
+*
+*  \param[in]  ppLinkedListHead - pointer to the head of the list.
+*
+*  \retval     None
+*
+******************************************************************************/
+void DestroyLinkedList(void **ppLinkedListHead);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* __LINKED_LIST_H__ */
